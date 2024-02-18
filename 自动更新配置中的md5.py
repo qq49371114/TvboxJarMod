4fd6edc17b72a0620dd0d4d89266b36f052da5b1
samuel <332118732@qq.com>
2024-02-18 09:08:23 +0800

    自动更新源

--- 自动更新配置中的md5.py
+++ 自动更新配置中的md5.py
@@ -7,6 +7,11 @@ import hashlib
 import os
 from git import Repo
 
+# 需更新的json文件名后缀，不含后缀名，如feimao-mod 或feimao-mod-cn
+suffix_list = ['-mod', '-mod-cn']
+current_path = os.path.dirname(os.path.abspath(__file__))
+conf_path = os.path.join(current_path, 'conf')
+print(current_path)
 
 # 更新json
 def update_json_md5():
@@ -17,33 +22,35 @@ def update_json_md5():
             json_md5 = ''
             with open('./jar/' + jar_file, 'rb') as f:
                 jar_md5 = hashlib.md5(f.read()).hexdigest()
-            # 生成json路径
-            json_path = jar_file.replace(".jar", "")
-            if json_path.lower().startswith('panda'):
-                json_path = 'feimao'
-            json_path += '-mod'
-            json_path = './conf/' + json_path + '.json'
-            with open(json_path, 'r', encoding='utf-8') as f:
-                try:
-                    data = json.load(f)
-                    json_spider = json_md5 = data['spider']
-                    if json_spider.find(";md5;") != -1:
-                        json_md5 = json_spider.split(';md5;')[1]
-                        if json_md5 == jar_md5:
-                            continue
-                        data['spider'] = json_spider.split(';md5;')[0] + ';md5;' + jar_md5
-                    else:
-                        json_spider += ';md5;' + jar_md5
-                    jar_name_json = json_spider.split(';md5;')[0].split('/')[-1]
-                    if jar_name_json != jar_file:
-                        json_spider = json_spider.replace(jar_name_json, jar_file)
-                    if json_md5 != jar_md5:
-                        data['spider'] = json_spider.split(';md5;')[0] + ';md5;' + jar_md5
-                    with open(json_path, 'w', encoding='utf-8') as f:
-                        json.dump(data, f, indent=4, ensure_ascii=False)
-                        print(json_path, 'md5值更新成功')
-                except Exception as e:
-                    print(e)
+            # 遍历生成json文件路径，并更新json
+            for suffix in suffix_list:
+                # 生成json路径
+                json_path = jar_file.replace(".jar", "")
+                if json_path.lower().startswith('panda'):
+                    json_path = 'feimao'
+                json_path = json_path + suffix + '.json'
+                json_path = os.path.join(conf_path, json_path)
+                with open(json_path, 'r', encoding='utf-8') as f:
+                    try:
+                        data = json.load(f)
+                        json_spider = json_md5 = data['spider']
+                        if json_spider.find(";md5;") != -1:
+                            json_md5 = json_spider.split(';md5;')[1]
+                            if json_md5 == jar_md5:
+                                continue
+                            data['spider'] = json_spider.split(';md5;')[0] + ';md5;' + jar_md5
+                        else:
+                            json_spider += ';md5;' + jar_md5
+                        jar_name_json = json_spider.split(';md5;')[0].split('/')[-1]
+                        if jar_name_json != jar_file:
+                            json_spider = json_spider.replace(jar_name_json, jar_file)
+                        if json_md5 != jar_md5:
+                            data['spider'] = json_spider.split(';md5;')[0] + ';md5;' + jar_md5
+                        with open(json_path, 'w', encoding='utf-8') as f:
+                            json.dump(data, f, indent=4, ensure_ascii=False)
+                            print(json_path, 'md5值更新成功')
+                    except Exception as e:
+                        print(e)
 
 # 自动git push多仓源配置json文件到远程仓库
 def git_push(repo_path):
